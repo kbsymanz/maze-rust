030414eef8cf6e8c9dea6111c41e46b6fab0bdb1
Kurt Symanzik <kurt@kbsymanzik.org>
2017-04-15 11:16:45 +0800

    Added command-line options.

---
+++ README.md
@@ -0,0 +1,20 @@
+# Maze-Rust
+
+A maze generation program written in Rust.
+
+## Features
+
+- Generates mazes in PNG and/or JSON format.
+- Saves to files with unique UUID file names.
+- Batch mode allows generating many mazes at once.
+- Specify the size of the maze to generate.
+- Uses the recursive backtracker algorithm.
+
+
+## Credits
+
+- A thank you to Tomas Korcak and his [Maze](https://github.com/korczis/maze-rs)
+project for some of the ideas that found their way into this project. As my
+first Rust project, I gained a lot by looking at Tomas' code.
+- Another thank you to Jamis Buck and his book, "Mazes for Programmers: Code
+Your Own Twisty Lttle Passages", for the recursive backtracker algorithm.
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 /// Implements the Recursive Backtracker algorithm for
 /// maze generation.
 
+extern crate clap;
+
 // Local
 extern crate kbsymanz_maze;
 
@@ -8,18 +10,62 @@ extern crate kbsymanz_maze;
 use kbsymanz_maze::grid;
 
 // Third-party
+use clap::{App, Arg};
 use std::collections::HashSet;
 
 #[cfg(not(test))]
 #[allow(unused_variables)]
 fn main() {
-    // TODO: 1. Accept command line parameters for batch, width, height, file format, etc.
-    let num_mazes = 1;
-    println!("Mazes generated:");
+    let matches = App::new("Maze Generator")
+                    .version("0.2.0")
+                    .author("Kurt Symanzik <kurt@kbsymanzik.org>")
+                    .about("Generates mazes")
+                    .arg(Arg::with_name("batch")
+                         .short("b")
+                         .long("batch")
+                         .help("Batch mode generates many mazes at once.")
+                         .takes_value(true))
+                    .arg(Arg::with_name("json")
+                         .short("j")
+                         .long("json")
+                         .help("Output the maze(s) in JSON format.")
+                         .takes_value(false))
+                    .arg(Arg::with_name("png")
+                         .short("p")
+                         .long("png")
+                         .help("Output the maze(s) in PNG format.")
+                         .takes_value(false))
+                    .arg(Arg::with_name("size")
+                         .short("s")
+                         .long("size")
+                         .help("Size of the maze, i.e. number of cells wide and high.")
+                         .takes_value(true))
+                    .get_matches();
+
+    // Number of mazes to generate, defaults to one.
+    let nbr_mazes = matches.value_of("batch").unwrap_or("1");
+    let num_mazes = nbr_mazes.parse::<i32>().unwrap_or(1);
+
+    // Output PNG and/or JSON, defaults to PNG.
+    let output_json: bool = matches.is_present("json");
+    let mut output_png: bool = matches.is_present("png");
+    if ! output_json && ! output_png {
+        output_png = true;
+    }
+
+    // Size of the maze, defaults to 40 x 40.
+    let size = matches.value_of("size").unwrap_or("40").parse::<usize>().unwrap_or(40);
+
+    print!("Generating {} maze", num_mazes);
+    if num_mazes > 1 {
+        print!("s");
+    }
+    println!(":");
+
     for i in 0..num_mazes {
         let mut grid = grid::Grid {
-            width: 40,
-            height: 40,
+            width: size,
+            height: size,
             cells: vec![],
             back_track: vec![],
             visited: HashSet::new(),
@@ -27,7 +73,7 @@ fn main() {
         grid.init();
         grid.carve(0, 0);
         grid.sanity();
-        grid.save(true, true);
+        grid.save(output_png, output_json);
     }
     println!("");
     println!("Created {} mazes.", num_mazes);
